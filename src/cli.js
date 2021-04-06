626a5822ae110c6a996712cb38df8af28c80db44
ikaverin <kaverin1990@gmail.com>
2021-04-06 20:02:54 +0300

    linted js files

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,5 @@
 import readlineSync from 'readline-sync';
-export const userName = readlineSync.question('May I have your name? ');
+
+const userName = readlineSync.question('May I have your name? ');
+
+export default userName;
